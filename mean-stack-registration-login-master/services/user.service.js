6b1064f01d354e7cd4b9861abedfc3a9a11f61cd
Don Vu <don.vu.22@gmail.com>
2017-10-17 18:05:50 -0400

    added email field front-end to back-end

--- mean-stack-registration-login-master/services/user.service.js
+++ mean-stack-registration-login-master/services/user.service.js
@@ -121,6 +121,7 @@ function update(_id, userParam) {
             firstName: userParam.firstName,
             lastName: userParam.lastName,
             username: userParam.username,
+            email: userParam.email
         };
 
         // update password if it was entered
